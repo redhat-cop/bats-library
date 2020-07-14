3ab8572c093e4f79e34516c43af89d357dd3d3b8
garethahealy <garethahealy@gmail.com>
2020-07-14 12:00:56 +0100

    trimmed whitespace

--- test/bats-support-clone.bash
+++ test/bats-support-clone.bash
@@ -2,4 +2,3 @@ if [[ ! -d "test/test_helper/bats-support" ]]; then
   # Download bats-support dynamically so it doesnt need to be added into source
   git clone https://github.com/ztombol/bats-support test/test_helper/bats-support --depth 1
 fi
-
