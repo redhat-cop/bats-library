a61aeb2aab217b67a501067b0bab8b319d405ff2
Gareth Healy <garethahealy@gmail.com>
2024-08-01 10:07:56 +0100

    added pre-commit

--- test/load.bash
+++ test/load.bash
@@ -1,6 +1,7 @@
+# shellcheck disable=all
+
 # Check all the dev-tools are installed
 command -v diff &> /dev/null || { echo >&2 'ERROR: diff not installed - Aborting'; exit 1; }
 command -v bc &> /dev/null || { echo >&2 'ERROR: bc not installed - Aborting'; exit 1; }
 
-# shellcheck source=../load.bash
 source "$(dirname "${BASH_SOURCE[0]}")/../load.bash"
